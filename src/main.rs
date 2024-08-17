7ba366dcaa39dec161926111e0b3ef4263a56a35
Andi Florea <alexflorea16000@gmail.com>
2024-08-17 22:46:15 +0300

    Leaf finding

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,49 @@ impl BTree {
             println!("Tree is empty");
         }
     }
+
+    // find leaf to insert into
+    // push in leaf
+    // if node not full => DONE
+    // if node full after push =>
+    // 1. find median
+    // 2. split node into left and right nodes (before and after median)
+    // 3. insert median into parent (may cause split and so on)
+    fn insert(&mut self, entry: BTreeNodeEntry) {
+        match &mut self.root {
+            Some(root) => {
+                // find leaf to insert into
+                let mut current_node = root;
+
+                // TODO: (2) extract and make recursive
+                while !current_node.is_leaf() {
+                    current_node = match current_node.get_greater_than_index(entry.key) {
+                        Ok(greater_index) => {
+                            if greater_index == 0 {
+                                current_node.left.as_mut().unwrap().as_mut()
+                            } else {
+                                let left_element = current_node.data.get_mut(greater_index - 1).unwrap();
+
+                                left_element.right.as_mut().unwrap()
+                            }
+                        }
+                        Err(BTreeNodeError::GreaterThanAll) => {
+                            let left_element = current_node.data.last_mut().unwrap();
+
+                            left_element.right.as_mut().unwrap()
+                        }
+                    }
+                }
+
+                // push in leaf
+                // TODO: (1) make it get inserted where it's supposed to
+                current_node.data.push(entry);
+            },
+            None => {
+                self.root = Some(BTreeNode::from_entry(entry));
+            }
+        }
+    }
 }
 
 struct BTreeNode {
@@ -24,11 +67,22 @@ struct BTreeNode {
     data: Vec<BTreeNodeEntry>,
 }
 
+enum BTreeNodeError {
+    GreaterThanAll
+}
+
 impl BTreeNode {
     fn new() -> Self {
         Self {
             left: None,
             data: vec![],
+        }
+    }
+
+    fn from_entry(entry: BTreeNodeEntry) -> Self {
+        Self {
+            left: None,
+            data: vec![entry],
         }
     }
 
@@ -55,6 +109,30 @@ impl BTreeNode {
 
         res
     }
+
+    fn is_leaf(&self) -> bool {
+        if let Some(_) = &self.left {
+            return false;
+        }
+
+        for entry in &self.data {
+            if let Some(_) = &entry.right {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn get_greater_than_index(&self, search_key: i64) -> Result<usize, BTreeNodeError> {
+        for (i, entry) in self.data.iter().enumerate() {
+            if entry.key > search_key {
+                return Ok(i);
+            }
+        }
+
+        Err(BTreeNodeError::GreaterThanAll)
+    }
 }
 
 struct BTreeNodeEntry {
@@ -74,23 +152,16 @@ impl BTreeNodeEntry {
 }
 
 fn main() {
-    // node 1 init
-    let mut node1 = BTreeNode::new();
+    // entries
     let entry1 = BTreeNodeEntry::new(1, 1, None);
-    let entry3 = BTreeNodeEntry::new(3, 3, None);
-    node1.data.push(entry1);
-    node1.data.push(entry3);
-
-    // node 2 init
-    let mut node2 = BTreeNode::new();
     let entry2 = BTreeNodeEntry::new(2, 2, None);
-    node2.data.push(entry2);
-    node1.left = Some(Box::new(node2));
+    let entry3 = BTreeNodeEntry::new(3, 3, None);
 
     // tree init
     let mut tree = BTree::new(3, None);
-    tree.root = Some(node1);
-
+    tree.insert(entry2);
+    tree.insert(entry1); // SHOULD INSERT ORDERED
+    tree.insert(entry3); // SHOULD SPLIT
 
     tree.print();
 }
