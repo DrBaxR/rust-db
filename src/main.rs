9be1a8a5077893a0a3240ca87310e81a46d56c2b
Andi Florea <alexflorea16000@gmail.com>
2024-08-25 15:10:20 +0300

    Initial commit (part 2)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use b_tree::{node::BTreeNodeEntry, BTree};
-
-pub mod b_tree;
-
 fn main() {
-    // entries
-    let entry1 = BTreeNodeEntry::new(1, 1, None);
-    let entry2 = BTreeNodeEntry::new(2, 2, None);
-    let entry3 = BTreeNodeEntry::new(3, 3, None);
-
-    // tree init
-    let mut tree = BTree::new(3, None);
-    tree.insert(entry3);
-    tree.insert(entry1);
-    tree.insert(entry2);
+    println!("Hello World!");
 }
