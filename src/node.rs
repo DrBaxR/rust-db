ba0d91d81bff619900772da7f569b4e6090ca65a
Alexandru Florea <alexandru.florea@hibyte.ro>
2024-08-26 18:12:16 +0300

    Node is parent of method

--- src/node.rs
+++ src/node.rs
@@ -30,11 +30,20 @@ impl Node {
         todo!()
     }
 
+    // determines if self is the parent of node
+    pub fn is_parent_of(&self, node: &Node) -> bool {
+        self.edges
+            .iter()
+            .filter(|op| op.is_some())
+            .map(|op| op.as_ref().unwrap())
+            .any(|child| std::ptr::eq(child, node))
+    }
+
     pub fn is_full(&self) -> bool {
         self.keys.len() >= 2 * self.b - 1
     }
 
-    // panics if trying to find a key in empty node 
+    // panics if trying to find a key in empty node
     pub fn find_leaf_for(&self, key: usize) -> &Node {
         todo!("implementation below is not correct according to the updated struct")
         // if self.is_leaf() {
@@ -57,7 +66,7 @@ impl Node {
         todo!()
     }
 
-    // push to current node, also taking care of the 
+    // push to current node, also taking care of the
     // should be able to handle case where self is empty
     pub fn push_with_children(&mut self, split: NodeSplit) -> Node {
         todo!()
