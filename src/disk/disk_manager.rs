d07f5e94afb8867107d7a7e819819e78dc372d8d
Andi Florea <alexflorea16000@gmail.com>
2024-10-02 20:15:45 +0300

    Implemented missing disk manager functionality

--- src/disk/disk_manager.rs
+++ src/disk/disk_manager.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read, os::unix::fs::FileExt};
+use std::{fs::File, os::unix::fs::FileExt};
 
 use crate::{DB_DEFAULT_SIZE, DB_PAGE_SIZE};
 
@@ -10,6 +10,7 @@ pub struct DiskManager {
 }
 
 impl DiskManager {
+    /// Create a new `DiskManager`. The `db_file_path` should be a Unix-like path (no Windows support atm).
     pub fn new(db_file_path: String) -> Self {
         let db_file = File::options()
             .read(true)
@@ -19,17 +20,24 @@ impl DiskManager {
             .expect("Database file couldn't be opened");
         let pages_capacity = DB_DEFAULT_SIZE;
 
-        // TODO: only set file size if file was created, otherwise might lose data
-        db_file
-            .set_len(pages_capacity as u64 * DB_PAGE_SIZE as u64)
-            .expect("Database file not opened for writing while initializing");
-
-        Self {
+        let new_dm = Self {
             db_file,
             pages_capacity,
+        };
+
+        let default_db_size = pages_capacity as u64 * DB_PAGE_SIZE as u64;
+        if new_dm.get_file_size() < default_db_size {
+            // resize db file in case it was just created
+            new_dm
+                .db_file
+                .set_len(default_db_size)
+                .expect("Database file not opened for writing while initializing");
         }
+
+        new_dm
     }
 
+    /// Increase disk size of the database file so it is capable of holding `pages_amount` pages. Will do nothing if database file is already large enough.
     pub fn increase_disk_size(&mut self, pages_amount: usize) {
         // TODO: make thread safe (probably just throw the file behind a mutex)
         if pages_amount < self.pages_capacity {
@@ -45,11 +53,14 @@ impl DiskManager {
             .expect("Database file not opened for writing while increasing disk size");
     }
 
-    pub fn read_page(&self, id: PageID) -> Vec<u8> {
+    /// Read page with `id`. Will return `None` if trying to read from an address that is beyond the allocated space.
+    pub fn read_page(&self, id: PageID) -> Option<Vec<u8>> {
         // TODO: make thread safe
         let offset = id * DB_PAGE_SIZE;
 
-        // TODO: check if read is beyond file size
+        if offset as u64 > self.get_file_size() {
+            return None;
+        }
 
         let mut buffer = [0 as u8; DB_PAGE_SIZE as usize];
         let bytes_read = self
@@ -61,9 +72,10 @@ impl DiskManager {
             buffer.to_vec().resize(DB_PAGE_SIZE as usize, 0);
         }
 
-        buffer.to_vec()
+        Some(buffer.to_vec())
     }
 
+    /// Write `data` to the page with `id`.
     pub fn write_page(&self, id: PageID, data: &[u8]) {
         // TODO: make thread safe
         let offset = id * DB_PAGE_SIZE;
@@ -75,10 +87,19 @@ impl DiskManager {
         self.db_file.sync_all().expect("Write flush to disk failed");
     }
 
+    /// Resize `data` to have the length of a database page. This either truncates the input (if larger than a database page), or pads it with `0`s (if smaller than a database file).
     fn pad_to_page_size(data: &[u8]) -> Vec<u8> {
         let mut res = data[..(DB_PAGE_SIZE as usize).min(data.len())].to_vec();
         res.resize(DB_PAGE_SIZE as usize, 0);
 
         res
+    }
+
+    /// Get the size of the database file on disk.
+    fn get_file_size(&self) -> u64 {
+        self.db_file
+            .metadata()
+            .expect("Failed to acquire metadata of the database file")
+            .len()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod tree;
 mod disk;
 
 fn main() {
-    let mut dm = DiskManager::new("test.db".to_string());
+    let dm = DiskManager::new("test.db".to_string());
 
     let mut page = "pee pee lol".as_bytes().to_vec();
     page.resize(DB_PAGE_SIZE as usize, 0);
@@ -17,6 +17,4 @@ fn main() {
     let mut page = "poo poo lmao".as_bytes().to_vec();
     page.resize(DB_PAGE_SIZE as usize, 0);
     dm.write_page(1, &page);
-
-    dm.increase_disk_size(17);
 }
