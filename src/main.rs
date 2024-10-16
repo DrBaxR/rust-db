fa5222567bc4044ab22dbf0a760561defbc1ce9d
Alexandru Florea <alexandru.florea@hibyte.ro>
2024-10-16 18:21:33 +0300

    Hash table header page structure

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,27 @@
-use std::{sync::Arc, thread};
-
-use config::DB_PAGE_SIZE;
-use disk::buffer_pool_manager::{BufferPoolManager, DiskRead, DiskWrite};
+use index::page::HashTableHeaderPage;
 
 mod b_tree;
 mod config;
 mod disk;
+mod index;
 
 fn main() {
-    let bpm = BufferPoolManager::new(String::from("db/test.db"), 2, 2);
-    let bpm = Arc::new(bpm);
-    let page_id1 = bpm.new_page();
-    let page_id2 = bpm.new_page();
-    let page_id3 = bpm.new_page();
+    let mut header_data = vec![];
+    for i in 0..4096 {
+        let val = if i % 4 == 3 {
+            if i > 2048 {
+                2
+            } else {
+                1
+            }
+        } else {
+            0
+        };
 
-    let mut handles = vec![];
-    for i in 0..30 {
-        // 10 threads of each category
-        let bpm = Arc::clone(&bpm);
-
-        let handle = thread::spawn(move || {
-            let category = i % 3 + 1;
-            let page_id = match category {
-                1 => page_id1,
-                2 => page_id2,
-                _ => page_id3,
-            };
-
-            let read_page = bpm.get_read_page(page_id);
-            let _ = read_page.read().clone();
-            drop(read_page);
-
-            let mut write_page = bpm.get_write_page(page_id);
-            let data = write_page.read();
-            let new_data = [data[0] + category; DB_PAGE_SIZE as usize].to_vec();
-            write_page.write(new_data);
-            drop(write_page);
-        });
-
-        handles.push(handle);
+        header_data.push(val);
     }
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    let header = HashTableHeaderPage::new(&header_data);
 
-    bpm.flush_all_pages();
+    println!("Max size: {}", header.max_size());
 }
