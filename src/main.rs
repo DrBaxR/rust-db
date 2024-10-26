abf3d9152a5197a13eba7046265d674a33e090f1
Andi Florea <alexflorea16000@gmail.com>
2024-10-26 14:19:47 +0300

    Code snippet proving not thread-safe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{sync::Arc, thread, time::Duration};
+use std::{fs::remove_file, sync::Arc, thread};
 
-use disk::{buffer_pool_manager::BufferPoolManager, disk_manager::PageID};
+use disk::buffer_pool_manager::BufferPoolManager;
 use index::disk_extendible_hash_table::DiskExtendibleHashTable;
 
 mod b_tree;
@@ -9,30 +9,60 @@ mod disk;
 mod index;
 
 fn main() {
-    let bpm = Arc::new(BufferPoolManager::new(String::from("db/test.db"), 100, 2));
-    let ht =
-        DiskExtendibleHashTable::<i32, i32>::new(Arc::clone(&bpm), 0, 4, String::from("index"));
+    let size = 5;
 
-    for i in 1..5000 {
-        ht.insert(i, i).unwrap();
-        ht.print();
-    }
-    
-    for i in 1..5000 {
-        ht.remove(i);
-        thread::sleep(Duration::from_millis(1));
-        ht.print();
+    {
+        let bpm = Arc::new(BufferPoolManager::new(String::from("db/test.db"), 100, 2));
+        let ht =
+            DiskExtendibleHashTable::<i32, i32>::new(Arc::clone(&bpm), 0, 4, String::from("index"));
+        let ht = Arc::new(ht);
+        single_thread(&ht, size);
     }
 
-    bpm.flush_all_pages();
+    remove_file("db/test.db").unwrap();
 
-    println!("FINAL TABLE STATE FROM DISK:");
-    read_from_disk(0, "index".to_string());
+    {
+        let bpm = Arc::new(BufferPoolManager::new(String::from("db/test.db"), 100, 2));
+        let ht =
+            DiskExtendibleHashTable::<i32, i32>::new(Arc::clone(&bpm), 0, 4, String::from("index"));
+        let ht = Arc::new(ht);
+        multi_thread(&ht, size);
+    }
 }
 
-fn read_from_disk(header_pid: PageID, name: String) {
-    let bpm = Arc::new(BufferPoolManager::new(String::from("db/test.db"), 2, 2));
-    let ht = DiskExtendibleHashTable::<i32, i32>::from_disk(bpm, header_pid, name);
+fn single_thread(ht: &Arc<DiskExtendibleHashTable<i32, i32>>, size: i32) {
+    for i in 0..size {
+        let start = i * 250;
+        let end = start + 250;
+
+        for i in start..end {
+            ht.insert(i, i).unwrap();
+        }
+    }
+
+    ht.print();
+}
+
+fn multi_thread(ht: &Arc<DiskExtendibleHashTable<i32, i32>>, size: i32) {
+    let mut handles = vec![];
+    for i in 0..size {
+        let ht = Arc::clone(&ht);
+
+        let handle = thread::spawn(move || {
+            let start = i * 250;
+            let end = start + 250;
+
+            for i in start..end {
+                ht.insert(i, i).unwrap();
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
     ht.print();
 }
