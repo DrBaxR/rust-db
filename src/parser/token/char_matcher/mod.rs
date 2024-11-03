64e1f0a482c041f7f110ce408e62bfc9fedc2bcf
Andi Florea <alexflorea16000@gmail.com>
2024-11-03 17:00:39 +0200

    FSM interface for character sequence matcher

--- src/parser/token/char_matcher/mod.rs
+++ src/parser/token/char_matcher/mod.rs
@@ -3,8 +3,12 @@ use std::{borrow::BorrowMut, collections::HashMap};
 #[cfg(test)]
 mod tests;
 
+// TODO: document how these structures work
 /// Can match a value to a character sequence. Internally, acts something like a trie, only internally
 /// explicitly specifies whether a node represents a match (containing the matched value) or not.
+///
+/// **Note:** The main disadvantage of this representation is that the tree-like structure means we can have
+/// no cycles, which leads to not being capable to have multiple characters transition to the same state.
 pub struct ChrSqMatcher<V> {
     root: ChrSqMatcherNode<V>,
 }
@@ -47,6 +51,43 @@ impl<V> ChrSqMatcher<V> {
 
         return current_node.value.as_ref();
     }
+
+    /// Returns a FSM using the root node as start state; and using internal trie data structure for transitioning
+    /// to other states.
+    pub fn as_fsm(&self) -> ChrSqMatcherFSM<V> {
+        ChrSqMatcherFSM::new(&self.root)
+    }
+}
+
+/// Uses same internal trie-like data structure as the `ChrSqMatcher`, only exposes an FSM-like interface for more
+/// efficient use in the context of a character-by-character use.
+///
+/// # Note
+/// The dissadvantage of this interface of the data structure is that it can only be used in read-only mode.
+pub struct ChrSqMatcherFSM<'a, V> {
+    state: &'a ChrSqMatcherNode<V>,
+}
+
+impl<'a, V> ChrSqMatcherFSM<'a, V> {
+    fn new(start_state: &'a ChrSqMatcherNode<V>) -> Self {
+        Self { state: start_state }
+    }
+
+    /// Applies the `c` transition the the FSM.
+    ///
+    /// # Errors
+    /// Will return `Err` if there is no state to transition to by applying `c`.
+    fn transition(&mut self, c: char) -> Result<(), ()> {
+        self.state = self.state.get_child(c).ok_or(())?;
+
+        Ok(())
+    }
+
+    /// Returns the value of the current state the machine is in. Will return `None` if the state has no value
+    /// associated to it.
+    fn current_value(&self) -> Option<&'a V> {
+        self.state.get_value()
+    }
 }
 
 /// If the value of the node is not `None`, then it means that it's a matching node.
@@ -71,5 +112,10 @@ impl<V> ChrSqMatcherNode<V> {
     /// Returns child for transition `c`.
     fn get_child(&self, c: char) -> Option<&ChrSqMatcherNode<V>> {
         self.children.get(&c)
+    }
+
+    /// Returns the value of the node.
+    fn get_value(&self) -> Option<&V> {
+        self.value.as_ref()
     }
 }
--- src/parser/token/char_matcher/tests.rs
+++ src/parser/token/char_matcher/tests.rs
@@ -1,12 +1,14 @@
 use super::ChrSqMatcher;
 
 #[test]
-fn empty () {
+fn empty() {
     let matcher = ChrSqMatcher::<usize>::new();
 
     assert!(matcher.get_match("abc").is_none());
     assert!(matcher.get_match("test123 wow").is_none());
-    assert!(matcher.get_match("this will never match you idiot").is_none());
+    assert!(matcher
+        .get_match("this will never match you idiot")
+        .is_none());
 }
 
 #[test]
@@ -32,7 +34,8 @@ fn add_post_create() {
 
 #[test]
 fn partial_matching() {
-    let mut matcher = ChrSqMatcher::with(vec![("in", 1), ("is", 2), ("instant", 3), ("instance", 4)]);
+    let mut matcher =
+        ChrSqMatcher::with(vec![("in", 1), ("is", 2), ("instant", 3), ("instance", 4)]);
 
     assert!(matcher.get_match("in").is_some());
     assert!(matcher.get_match("instant").is_some());
@@ -48,9 +51,38 @@ fn partial_matching() {
 #[test]
 fn matching_value() {
     let matcher = ChrSqMatcher::with(vec![("one", 1), ("two", 2), ("three", 3)]);
-    
+
     assert_eq!(matcher.get_match("one"), Some(&1));
     assert_eq!(matcher.get_match("two"), Some(&2));
     assert_eq!(matcher.get_match("three"), Some(&3));
     assert_eq!(matcher.get_match("four"), None);
+}
+
+#[test]
+fn fsm() {
+    let matcher = ChrSqMatcher::with(vec![
+        ("one", 1),
+        ("one hundred", 100),
+        ("one thousand", 1000),
+    ]);
+    let mut fsm = matcher.as_fsm();
+    assert_eq!(fsm.current_value(), None);
+
+    fsm.transition('o').unwrap();
+    fsm.transition('n').unwrap();
+    fsm.transition('e').unwrap();
+    assert_eq!(fsm.current_value(), Some(&1));
+    assert!(fsm.transition('e').is_err());
+
+    fsm.transition(' ').unwrap();
+    assert_eq!(fsm.current_value(), None);
+
+    fsm.transition('h').unwrap();
+    fsm.transition('u').unwrap();
+    fsm.transition('n').unwrap();
+    fsm.transition('d').unwrap();
+    fsm.transition('r').unwrap();
+    fsm.transition('e').unwrap();
+    fsm.transition('d').unwrap();
+    assert_eq!(fsm.current_value(), Some(&100));
 }
