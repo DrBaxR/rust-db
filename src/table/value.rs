b50692fd802093855fb3d6b88f46f896ec9cbd4a
Andi Florea <alexflorea16000@gmail.com>
2024-12-22 15:55:06 +0200

    Column values deserialization

--- src/table/value.rs
+++ src/table/value.rs
@@ -1,3 +1,5 @@
+use core::str;
+
 use super::schema::ColumnType;
 
 pub enum ColumnValue {
@@ -12,12 +14,12 @@ pub enum ColumnValue {
 }
 
 pub trait Value {
+    fn deserialize(data: &[u8]) -> Self;
     fn serialize(&self) -> Vec<u8>;
     fn storage_size(&self) -> usize;
 }
 
-// TODO: for all value structs also implement deserialization...
-
+#[derive(Debug, PartialEq)]
 pub struct BooleanValue {
     value: bool,
 }
@@ -30,8 +32,21 @@ impl Value for BooleanValue {
     fn storage_size(&self) -> usize {
         ColumnType::Boolean.size()
     }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::Boolean.size());
+
+        if data[0] == 0 {
+            Self { value: false }
+        } else if data[0] == 1 {
+            Self { value: true }
+        } else {
+            panic!("Invalid boolean value, can only be 0 or 1")
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct TinyIntValue {
     value: i8,
 }
@@ -44,8 +59,17 @@ impl Value for TinyIntValue {
     fn storage_size(&self) -> usize {
         ColumnType::TinyInt.size()
     }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::TinyInt.size());
+
+        Self {
+            value: data[0] as i8,
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct SmallIntValue {
     value: i16,
 }
@@ -58,8 +82,17 @@ impl Value for SmallIntValue {
     fn storage_size(&self) -> usize {
         ColumnType::SmallInt.size()
     }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::SmallInt.size());
+
+        Self {
+            value: i16::from_be_bytes([data[0], data[1]]),
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct IntegerValue {
     value: i32,
 }
@@ -77,10 +110,19 @@ impl Value for IntegerValue {
     fn storage_size(&self) -> usize {
         ColumnType::Integer.size()
     }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::Integer.size());
+
+        Self {
+            value: i32::from_be_bytes([data[0], data[1], data[2], data[3]]),
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct BigIntValue {
-    value: u64,
+    value: i64,
 }
 
 impl Value for BigIntValue {
@@ -98,10 +140,21 @@ impl Value for BigIntValue {
     }
 
     fn storage_size(&self) -> usize {
-        todo!()
+        ColumnType::BigInt.size()
+    }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::BigInt.size());
+
+        Self {
+            value: i64::from_be_bytes([
+                data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+            ]),
+        }
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct DecimalValue {
     value: f64,
 }
@@ -114,8 +167,19 @@ impl Value for DecimalValue {
     fn storage_size(&self) -> usize {
         ColumnType::Decimal.size()
     }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::Decimal.size());
+
+        Self {
+            value: f64::from_be_bytes([
+                data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+            ]),
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct TimestampValue {
     value: u64,
 }
@@ -137,8 +201,19 @@ impl Value for TimestampValue {
     fn storage_size(&self) -> usize {
         ColumnType::Timestamp.size()
     }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert_eq!(data.len(), ColumnType::Timestamp.size());
+
+        Self {
+            value: u64::from_be_bytes([
+                data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+            ]),
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct VarcharValue {
     value: String,
     length: usize,
@@ -152,12 +227,134 @@ impl Value for VarcharValue {
         let mut content: Vec<_> = self.value.bytes().collect();
         content.resize(self.length, b'\0');
 
-        bytes.append(&mut self.value.bytes().collect());
+        bytes.append(&mut content);
+        dbg!(bytes.len());
 
         bytes
     }
 
     fn storage_size(&self) -> usize {
         self.length + 4 // characters + u32
+    }
+
+    fn deserialize(data: &[u8]) -> Self {
+        assert!(data.len() > 4);
+        let length = u32::from_be_bytes([data[0], data[1], data[2], data[3]]) as usize;
+
+        assert_eq!(data.len(), length + 4);
+        let value = str::from_utf8(&data[4..])
+            .expect("Invalid string payload, potentially wrong cast")
+            .split('\0') // is it possible for UTF-8 characters to have some b0000_0000 byte in the middle of them?
+            .next()
+            .expect("String can't be split at \\0 character")
+            .to_string();
+
+        Self { value, length }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::table::value::{BigIntValue, DecimalValue, IntegerValue, TimestampValue};
+
+    use super::{BooleanValue, SmallIntValue, TinyIntValue, Value, VarcharValue};
+
+    #[test]
+    fn boolean_value_serialization_consistency() {
+        let value = BooleanValue { value: true };
+        let serialized = value.serialize();
+        let deserialized = BooleanValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn tiny_int_value_serialization_consistency() {
+        let value = TinyIntValue { value: 18 };
+        let serialized = value.serialize();
+        let deserialized = TinyIntValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn small_int_value_serialization_consistency() {
+        let value = SmallIntValue { value: -25639 };
+        let serialized = value.serialize();
+        let deserialized = SmallIntValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn integer_value_serialization_consistency() {
+        let value = IntegerValue { value: 1147483648 };
+        let serialized = value.serialize();
+        let deserialized = IntegerValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn big_int_value_serialization_consistency() {
+        let value = BigIntValue {
+            value: -8223372843128043239,
+        };
+        let serialized = value.serialize();
+        let deserialized = BigIntValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn decimal_value_serialization_consistency() {
+        let value = DecimalValue {
+            value: 234534563.890423,
+        };
+        let serialized = value.serialize();
+        let deserialized = DecimalValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn timestamp_value_serialization_consistency() {
+        let value = TimestampValue {
+            value: 17446744912301425290,
+        };
+        let serialized = value.serialize();
+        let deserialized = TimestampValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+    }
+
+    #[test]
+    fn varchar_value_serialization_consistency() {
+        let value = VarcharValue {
+            value: "this is some string".to_string(),
+            length: 255,
+        };
+        let serialized = value.serialize();
+        let deserialized = VarcharValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+
+        let value = VarcharValue {
+            value: "abc".to_string(),
+            length: 3,
+        };
+        let serialized = value.serialize();
+        let deserialized = VarcharValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
+
+        let value = VarcharValue {
+            value: "🍆🍆🍆🍆".to_string(),
+            length: 255,
+        };
+        let serialized = value.serialize();
+        let deserialized = VarcharValue::deserialize(&serialized);
+
+        assert_eq!(value, deserialized);
     }
 }
