596570563602f5c2434b39f1debc0b650a2fa507
Andi Florea <alexflorea16000@gmail.com>
2025-02-15 01:41:14 +0200

    Create table implementation

--- src/catalog/mod.rs
+++ src/catalog/mod.rs
@@ -1,6 +1,13 @@
-use std::{collections::HashMap, sync::atomic::AtomicU32};
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc,
+    },
+};
 
 use crate::{
+    disk::buffer_pool_manager::BufferPoolManager,
     index::Index,
     table::{schema::Schema, TableHeap},
 };
@@ -9,6 +16,7 @@ type OID = u32;
 
 // TODO: will need to be thread safe
 pub struct Catalog {
+    bpm: Arc<BufferPoolManager>,
     next_oid: AtomicU32,
     /// oid -> table info
     tables: HashMap<OID, TableInfo>,
@@ -21,23 +29,50 @@ pub struct Catalog {
 }
 
 impl Catalog {
-    fn new() -> Self {
-        todo!()
+    pub fn new(bpm: Arc<BufferPoolManager>) -> Self {
+        Self {
+            bpm,
+            next_oid: AtomicU32::new(0),
+            tables: HashMap::new(),
+            table_names: HashMap::new(),
+            indexes: HashMap::new(),
+            index_names: HashMap::new(),
+        }
     }
 
-    fn create_table(&mut self, name: &str, schema: Schema) -> Result<OID, ()> {
-        todo!()
+    pub fn create_table(&mut self, name: &str, schema: Schema) -> Result<&TableInfo, ()> {
+        if let Some(_) = self.table_names.get(name) {
+            return Err(());
+        }
+
+        let oid = self.next_oid.fetch_add(1, Ordering::SeqCst);
+        let heap = TableHeap::new(self.bpm.clone());
+        let table_info = TableInfo {
+            name: name.to_string(),
+            oid,
+            schema,
+            table: heap,
+        };
+
+        self.table_names.insert(name.to_string(), oid);
+        self.index_names.insert(name.to_string(), HashMap::new());
+        self.tables.insert(oid, table_info);
+
+        Ok(self.tables.get(&oid).unwrap())
     }
 
-    fn get_table_by_name(&self, name: &str) -> Option<&TableInfo> {
-        todo!()
+    pub fn get_table_by_name(&self, name: &str) -> Option<&TableInfo> {
+        match self.table_names.get(name) {
+            Some(oid) => self.tables.get(oid),
+            None => None,
+        }
     }
 
-    fn get_table_by_oid(&self, oid: OID) -> Option<&TableInfo> {
-        todo!()
+    pub fn get_table_by_oid(&self, oid: OID) -> Option<&TableInfo> {
+        self.tables.get(&oid)
     }
 
-    fn create_index(
+    pub fn create_index(
         &mut self,
         index_name: &str,
         table_name: &str,
@@ -49,20 +84,20 @@ impl Catalog {
         todo!()
     }
 
-    fn get_index_by_name(&self, index_name: &str, table_name: &str) -> Option<&IndexInfo> {
+    pub fn get_index_by_name(&self, index_name: &str, table_name: &str) -> Option<&IndexInfo> {
         todo!()
     }
 
-    fn get_index_by_oid(&self, oid: OID) -> Option<&IndexInfo> {
+    pub fn get_index_by_oid(&self, oid: OID) -> Option<&IndexInfo> {
         todo!()
     }
 
-    fn get_table_indexes(&self, name: &str) -> Vec<&IndexInfo> {
+    pub fn get_table_indexes(&self, name: &str) -> Vec<&IndexInfo> {
         todo!()
     }
 
-    fn get_table_names(&self) -> Vec<String> {
-        todo!()
+    pub fn get_table_names(&self) -> Vec<String> {
+        self.table_names.keys().cloned().collect()
     }
 }
 
